cead25e574f66b4bee6595dffb39b924c92a21ac
CarlosHV010101 <chedezv@comunidad.unam.mx>
2022-09-15 20:14:06 -0500

    Add Player model

---
+++ Squid Game/Model/Player.swift
@@ -0,0 +1,19 @@
+//
+//  Player.swift
+//  Squid Game
+//
+//  Created by Carlos Hernández on 15/09/22.
+//
+
+import Foundation
+
+class Player {
+    
+    let id: UUID = UUID()
+    let name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+}
