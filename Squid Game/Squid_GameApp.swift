954ebe0d696c5c261dae7fae3c10604396ab70a9
CarlosHV010101 <chedezv@comunidad.unam.mx>
2022-09-15 20:04:17 -0500

    Initial Commit

---
+++ Squid Game/Squid_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Squid_GameApp.swift
+//  Squid Game
+//
+//  Created by mac on 15/09/22.
+//
+
+import SwiftUI
+
+@main
+struct Squid_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
